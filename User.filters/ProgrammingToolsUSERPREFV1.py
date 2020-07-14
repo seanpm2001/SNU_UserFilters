01e9d619dd467a885a9d0e032b76f2f789476a01
Sean P. Myrick V19.0.3.1 <65933340+seanpm2001@users.noreply.github.com>
2020-07-13 18:01:50 -0700

    2 User preference scripts (PY)
    2 user preference script files, in Python 3.7 format.

---
+++ User.filters/ProgrammingToolsUSERPREFV1.py
@@ -0,0 +1,13 @@
+# User-setup preferences for SNU programming
+'''
+The user has the choice to store it locally (default) or on the server (do at your own risk (very minor privacy concern))
+'''
+print ("Reading user preferences for SNU programming...")
+themeDefault = True # Boolean value for the default interface
+themeCustom = False # Boolean value for the latest interface preference
+faviconCheck = True # Boolean value for custom favicons
+'''
+File version: 1 (October 18th 2019)
+Latest version: 1 (October 18th 2019)
+First version: 1 (October 18th 2019)
+'''
